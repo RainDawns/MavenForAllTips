634a912b2fa8f33b5032043366a62386c46be2e6
raindown <2071149217@qq.com>
2023-03-25 20:44:48 +0800

    just for test

---
+++ src/main/java/com/raindown/MavenForAllTipsMainApplication.java
@@ -0,0 +1,13 @@
+package com.raindown;
+
+/**
+ * @author: RainDown
+ * @description: TODO
+ * @date: 2023/3/25 20:42
+ * @version: 1.0
+ */
+public class MavenForAllTipsMainApplication {
+    public static void main(String[] args) {
+        System.out.println("just for a test");
+    }
+}
